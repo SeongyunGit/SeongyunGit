77a8757ee3a2ff3d4c65bfa7d8780a8ce8f1e466
Seongyun Jo <okks3579@gmail.com>
2025-02-23 16:18:43 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+        int cnt = 0;
+        while(num <= n) {
+            num += (w - ((num-1) % w) -1) * 2 + 1;
+            cnt++;
+        }
+
+        // 반환
+        int answer = cnt;
+        return answer;
+    }
+}
