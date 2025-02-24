ec8907b335fa8f62848f33475120659e430dd46d
Seongyun Jo <okks3579@gmail.com>
2025-02-24 14:58:16 +0900

    [level 2] Title: H-Index, Time: 0.97 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int answer = 0;
+        for (int i=0;i<citations.length;i++) {
+            int h = citations.length-i;
+            if (h<=citations[i]) {
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
