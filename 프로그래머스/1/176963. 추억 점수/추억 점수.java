e392e28391d0e3e76dd38d77a25ccea9f218d543
Seongyun Jo <okks3579@gmail.com>
2025-03-10 21:40:51 +0900

    [level 1] Title: 추억 점수, Time: 17.54 ms, Memory: 94.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        int idx = 0;
+        for (int i=0;i<photo.length;i++) {
+            int count = 0;
+            for (int j=0;j<photo[i].length;j++) {
+                for (int k=0;k<name.length;k++) {
+                    if (photo[i][j].equals(name[k])) {
+                        count += yearning[k];
+                    }   
+                }
+            }
+            answer[idx] = count;
+            idx++;
+        }
+        return answer;
+    }
+}
